148e21a8fb24cdcab8d9de0aecdb6269eef49c55
Shantanu Barua <shantanu.barua.sustipe@gmail.com>
2025-04-11 11:16:45 +0930

    Completed preprocessing to numeric variables

--- preprocessing.py
+++ preprocessing.py
@@ -1,12 +1,27 @@
 import pandas as pd
 import numpy as np
 import statsmodels as sm
+from sklearn.preprocessing import LabelEncoder
 
 df_avn = pd.read_excel("Avian data.xlsx")
 
 # print(df_avn.dtypes)
 # print(df_avn.columns, "\n")
 # print(df_avn.info)
+
+### Label encoding of Region ###
+le = LabelEncoder()
+df_avn['Region_Code'] = le.fit_transform(df_avn['Region'])
+# Drop original Region column
+df_avn.drop('Region', axis=1, inplace=True)
+
+# Check mapping of region codes
+region_mapping = dict(zip(le.classes_, le.transform(le.classes_)))
+print("Region to Code Mapping:")
+for region, code in region_mapping.items():
+    print(f"{region}: {code}")
+
+print("\n---------------\n")
 
 ### One-Hot Encoding of Age variable ###
 
@@ -27,3 +42,4 @@ df_avn_age_yms = df_avn_age_ym.drop(["Year", "Month"], axis=1, inplace=True)
 df_avn_age_ym = df_avn_age_ym[[col for col in df_avn_age_ym.columns if col != "Death"] + ["Death"]]
 
 print(df_avn_age_ym)
+
